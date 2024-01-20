ce97f8007725208bdf317f0cb632e3c8930f6870
shahon922 <shahon922@naver.com>
2024-01-20 21:03:14 +0900

    캐릭터 이동(영상과 똑같이 만들어 놓음 수정 준비)

---
+++ Town/Assets/Scripts/Entities/TownMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownMovement : MonoBehaviour
+{
+    private TownCharacterController _controller;
+
+    private Vector2 _movementDirection = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TownCharacterController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(_movementDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        _rigidbody.velocity = direction;
+    }
+    
+}
