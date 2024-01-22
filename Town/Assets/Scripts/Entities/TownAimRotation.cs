cd3aa8aad4c464fa31788a798a9315c9c8538521
shahon922 <shahon922@naver.com>
2024-01-22 15:59:09 +0900

    캐릭터 방향 전환

---
+++ Town/Assets/Scripts/Entities/TownAimRotation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer armRenderer;
+    [SerializeField] private Transform armPivot;
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TownCharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TownCharacterController>();
+    }
+
+    void Start()
+    {
+        _controller.OnLookEvent += OnAim;
+    }
+
+   public void OnAim(Vector2 newAimDirection)
+    {
+        RotateArm(newAimDirection);
+    }
+
+    private void RotateArm(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        armRenderer.flipY = Mathf.Abs(rotZ) > 90f;
+        characterRenderer.flipX = armRenderer.flipY;
+        armPivot.rotation = Quaternion.Euler(0,0,rotZ) ;
+    }
+}
