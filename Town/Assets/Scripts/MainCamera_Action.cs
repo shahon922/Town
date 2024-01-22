71457fd4ad77a45dffcec1a177c1699ac3022a4a
shahon922 <shahon922@naver.com>
2024-01-22 19:37:11 +0900

    주석 달음

--- Town/Assets/Scripts/MainCamera_Action.cs
+++ Town/Assets/Scripts/MainCamera_Action.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class MainCamera_Action : MonoBehaviour
 {
+    // 따라 다닐 타겟을 지정
     public GameObject Target;
 
+    // 시작할 때 카메라 위치
     public float offsetX = 0.0f;
     public float offsetY = 10.0f;
     public float offsetZ = -10.0f;
 
+    // 이동할 때 카메라 속도
     public float CameraSpeed = 10.0f;
+
+    // 
     Vector3 TargetPos;
 
     private void FixedUpdate()
